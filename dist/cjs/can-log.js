028e6730d5b00ca10509276871df44fc017b98b3
Justin Meyer <justinbmeyer@gmail.com>
2018-01-26 13:35:46 -0600

    1.0.0

---
+++ dist/cjs/can-log.js
@@ -0,0 +1,37 @@
+/*can-log@0.1.2#can-log*/
+'use strict';
+exports.warnTimeout = 5000;
+exports.logLevel = 0;
+exports.warn = function () {
+    var ll = this.logLevel;
+    if (ll < 2) {
+        if (typeof console !== 'undefined' && console.warn) {
+            this._logger('warn', Array.prototype.slice.call(arguments));
+        } else if (typeof console !== 'undefined' && console.log) {
+            this._logger('log', Array.prototype.slice.call(arguments));
+        }
+    }
+};
+exports.log = function () {
+    var ll = this.logLevel;
+    if (ll < 1) {
+        if (typeof console !== 'undefined' && console.log) {
+            this._logger('log', Array.prototype.slice.call(arguments));
+        }
+    }
+};
+exports.error = function () {
+    var ll = this.logLevel;
+    if (ll < 1) {
+        if (typeof console !== 'undefined' && console.error) {
+            this._logger('error', Array.prototype.slice.call(arguments));
+        }
+    }
+};
+exports._logger = function (type, arr) {
+    try {
+        console[type].apply(console, arr);
+    } catch (e) {
+        console[type](arr);
+    }
+};
